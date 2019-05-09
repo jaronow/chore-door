8b69c66c7bb6ede952f26cd74d43fb92c35dde93
Will Aronow <john.aronow@gmail.com>
2019-05-09 15:14:54 -0700

    added chore bot image and action to show it when clicked on closed door

--- java/script.js
+++ java/script.js
@@ -1,5 +1,6 @@
 let doorImage1 = document.getElementById('door1');
+const botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
 
 doorImage1.onclick = () => {
-
+  doorImage1.src = botDoorPath;
 };
